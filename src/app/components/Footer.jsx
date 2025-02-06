17bc3610bace04628350ccd21e7331c56c53f851
Didar Abbas <didarabbas8@gmail.com>
2025-02-06 18:46:39 +0530

    Header and Footer completed

---
+++ src/app/components/Footer.jsx
@@ -0,0 +1,52 @@
+'use client'
+import { Footer } from "flowbite-react";
+import { BsDribbble, BsFacebook, BsGithub, BsInstagram, BsTwitter } from "react-icons/bs";
+
+
+export default function FooterComp() {
+  return (
+    <Footer container className="bg-emerald-100">
+      <div className="w-full">
+        <div className="grid w-full justify-between sm:flex sm:justify-between md:flex md:grid-cols-1">
+          <div className="my-2">
+            <Footer.Brand src="Logo.png" alt="Logo" className="h-[10rem] rounded-lg" />
+          </div>
+          <div className="grid grid-cols-2 gap-8 sm:mt-4 sm:grid-cols-3 sm:gap-6">
+            <div className="">
+              <Footer.Title title="about" className="text-black font-bold" />
+              <Footer.LinkGroup col>
+                <Footer.Link href="#" className="text-black">Flowbite</Footer.Link>
+                <Footer.Link href="#" className="text-black">Tailwind CSS</Footer.Link>
+              </Footer.LinkGroup>
+            </div>
+            <div>
+              <Footer.Title title="Follow us" className="text-black font-bold" />
+              <Footer.LinkGroup col>
+                <Footer.Link href="/#" className="text-black">Github</Footer.Link>
+                <Footer.Link href="#" className="text-black">Discord</Footer.Link>
+              </Footer.LinkGroup>
+            </div>
+            <div>
+              <Footer.Title title="Legal" className="text-black font-bold" />
+              <Footer.LinkGroup col>
+                <Footer.Link href="#" className="text-black">Privacy Policy</Footer.Link>
+                <Footer.Link href="#" className="text-black">Terms &amp; Conditions</Footer.Link>
+              </Footer.LinkGroup>
+            </div>
+          </div>
+        </div>
+        <Footer.Divider />
+        <div className="w-full sm:flex sm:items-center sm:justify-between">
+          <Footer.Copyright href="#" by="Didar Abbas" year={2025} className="text-black font-bold"/>
+          <div className="mt-4 flex space-x-6 sm:mt-0 sm:justify-center">
+            <Footer.Icon href="#" icon={BsFacebook} className="text-black" />
+            <Footer.Icon href="#" icon={BsInstagram} className="text-black"/>
+            <Footer.Icon href="#" icon={BsTwitter} className="text-black"/>
+            <Footer.Icon href="#" icon={BsGithub} className="text-black"/>
+            <Footer.Icon href="#" icon={BsDribbble} className="text-black"/>
+          </div>
+        </div>
+      </div>
+    </Footer>
+  )
+}
---
+++ src/app/components/Header.jsx
@@ -0,0 +1,33 @@
+'use client'
+import { Button, Navbar, TextInput } from "flowbite-react"
+import Link from "next/link"
+import { useEffect,useState } from "react";
+import { usePathname } from "next/navigation";
+
+const Header = () => {
+    const path = usePathname();
+  return (
+    <Navbar className="bg-emerald-100 h-[fit-content] ">
+      <Link href='/' className="font-bold text-lg sm:text-xl dark:text-white w-[27%] sm:w-[20%] md:w-[8%]">
+        <img src="./Logo.png" alt="Logo Image" className="rounded-lg"/>
+      </Link>
+        <Navbar.Toggle className="bg-gray-100" />
+      <Navbar.Collapse>
+        <Link href='/'>
+          <Navbar.Link active={path === '/'} as={'div'}>Home</Navbar.Link>
+        </Link>
+        <Link href='/about'>
+          <Navbar.Link active={path === '/about'} as={'div'}>About</Navbar.Link>
+        </Link>
+        <Link href='/projects'>
+          <Navbar.Link active={path === '/projects'} as={'div'}>Projects</Navbar.Link>
+        </Link>
+        <Link href='/dashboard'>
+          <Navbar.Link active={path === '/dashboard'} as={'div'}>Dashboard</Navbar.Link>
+        </Link>
+      </Navbar.Collapse>
+    </Navbar>
+  )
+}
+
+export default Header
